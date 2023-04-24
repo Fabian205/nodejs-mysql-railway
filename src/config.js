fdf9d1d497f3288fcceb3b915409b8429be4ebbc
Fabian205 <fabaguilar205@gmail.com>
2023-04-24 16:21:24 -0500

    prueba conex 02

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 const PORT = process.env.PORT || 3000
 
-/* const DB_HOST = process.env.DB_HOST || 'localhost'
+const DB_HOST = process.env.DB_HOST || 'localhost'
 const DB_USER = process.env.DB_USER || 'root'
 const DB_PASSWORD = process.env.DB_PASSWORD || 'nobasys2411'
 const DB_NAME = process.env.DB_NAME || 'productsdb'
-const DB_PORT = process.env.DB_PORT || 3306 */
+const DB_PORT = process.env.DB_PORT || 3306
 
-const DB_HOST = process.env.DB_HOST || 'containers-us-west-57.railway.app'
+/* const DB_HOST = process.env.DB_HOST || 'containers-us-west-57.railway.app'
 const DB_USER = process.env.DB_USER || 'root'
 const DB_PASSWORD = process.env.DB_PASSWORD || 'IlQ4h2cJyiXS8lzaDWVS'
 const DB_NAME = process.env.DB_NAME || 'railway'
-const DB_PORT = process.env.DB_PORT || 7702
+const DB_PORT = process.env.DB_PORT || 7702 */
 
 module.exports = {
 	PORT: PORT,
